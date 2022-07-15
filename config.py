1ea3fd2588dadf7549ecf15d1493f0ac15879afb
MohamedTanimus <91375689+MohamedTanimus@users.noreply.github.com>
2022-07-15 12:52:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "2025583789:AAEsjpi7wmcWXvh20qb5PsoaUNl3WZjNvco"
+BOT_TOKEN = "5593144986:AAEYxTWwPUoCp4bzpO6yl49nbNlY3KW-Ay8"
 APP_URL = "https://mdkkdkdkdkdkjd.herokuapp.com/" + BOT_TOKEN
 
 
