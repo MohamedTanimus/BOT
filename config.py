b123dfec1a047f37c1f4e3b0399d82a335c4d819
MohamedTanimus <91375689+MohamedTanimus@users.noreply.github.com>
2022-07-15 12:54:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "5593144986:AAEYxTWwPUoCp4bzpO6yl49nbNlY3KW-Ay8"
-APP_URL = "https://mdkkdkdkdkdkjd.herokuapp.com/" + BOT_TOKEN
+APP_URL = "https://botgiel10k.herokuapp.com/" + BOT_TOKEN
 
 
